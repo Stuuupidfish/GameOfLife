6e208ed6e8772b50d7c5e8f0e84ee3d5cd09805f
Stuuupidfish <elainexyz2006@gmail.com>
2026-04-03 14:25:18 -0400

    whoops i forgot the asyncio stuff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #importing libraries
-import pygame, sys, copy
+import pygame, sys, copy, asyncio
 
 #i gotta run this in 3.13 so this is a note to myself:
 # py -3.13 GameOfLife.py
@@ -81,8 +81,8 @@ speeds = [0.25, 0.5, 1, 2] #updates per second
 curSpeed = 0; #index for speeds list
 
 # Timer for grid updates
-update_timer = 0.0
-update_interval = 1.0 / speeds[curSpeed]  # seconds between updates
+updateTimer = 0.0
+updateInterval = 1.0 / speeds[curSpeed]  # seconds between updates
 
 pause = False
 
@@ -94,61 +94,67 @@ class Button:
 
     def draw(self, surface):
         pygame.draw.rect(surface, (200, 200, 200), self.rect)
-        text_surf = self.font.render(self.text, True, (0, 0, 0))
-        text_rect = text_surf.get_rect(center=self.rect.center)
-        surface.blit(text_surf, text_rect)
+        textSurf = self.font.render(self.text, True, (0, 0, 0))
+        textRect = textSurf.get_rect(center=self.rect.center)
+        surface.blit(textSurf, textRect)
 
-    def is_clicked(self, pos):
+    def isClicked(self, pos):
         return self.rect.collidepoint(pos)
+
 
 #while the program is running-- continuously updates
 running = True
-while running: #aka run == True
-    dt = clock.tick(60) / 1000.0  # Run at 60 FPS, get delta time in seconds
-    update_timer += dt
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:  #left mouse button
-                x, y = pygame.mouse.get_pos()
-                # // is for integer division btw
-                gridX = x // BLOCK_WIDTH  # Calculate which column in the grid
-                gridY = y // BLOCK_WIDTH  # Calculate which row in the grid
-                #update the color of the clicked square
-                state = grid[gridY][gridX];
-                if state == 0:
-                    grid[gridY][gridX] = 1
-                    cells[(gridX, gridY)] = True  # Add to live cells list
+async def main():
+    global running, updateTimer, updateInterval
+    while running: #aka run == True
+        dt = clock.tick(60) / 1000.0  # Run at 60 FPS, get delta time in seconds
+        updateTimer += dt
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  #left mouse button
+                    x, y = pygame.mouse.get_pos()
+                    # // is for integer division btw
+                    gridX = x // BLOCK_WIDTH  # Calculate which column in the grid
+                    gridY = y // BLOCK_WIDTH  # Calculate which row in the grid
+                    #update the color of the clicked square
+                    state = grid[gridY][gridX];
+                    if state == 0:
+                        grid[gridY][gridX] = 1
+                        cells[(gridX, gridY)] = True  # Add to live cells list
+                    else:
+                        grid[gridY][gridX] = 0
+                        cells.pop((gridX, gridY))  # Remove from live cells list
+
+        
+
+        #refreshes screen
+        #screen.fill((0, 0, 0))-- refresh is replaced with the below that will refresh with not a blank slate but its current state
+        for row in range(GRID_HEIGHT):
+            for column in range(GRID_WIDTH):
+                color = grid[row][column]
+                if color != 0:  # Only draw if the cell is not empty
+                    pygame.draw.rect(screen, (255,255,255), pygame.Rect(column * BLOCK_WIDTH, row * BLOCK_WIDTH, BLOCK_WIDTH, BLOCK_WIDTH))
                 else:
-                    grid[gridY][gridX] = 0
-                    cells.pop((gridX, gridY))  # Remove from live cells list
+                    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(column * BLOCK_WIDTH, row * BLOCK_WIDTH, BLOCK_WIDTH, BLOCK_WIDTH))
+        drawGridLines()
 
-    
+        pygame.display.update()
+        
+        # Update grid only when enough time has passed
+        if updateTimer >= updateInterval:
+            updateGrid()
+            updateTimer = 0.0
+            # Recalculate interval if speed changed
+            updateInterval = 1.0 / speeds[curSpeed]
+        
+        await asyncio.sleep(0)  # Yield control to allow other tasks to run
 
-    #refreshes screen
-    #screen.fill((0, 0, 0))-- refresh is replaced with the below that will refresh with not a blank slate but its current state
-    for row in range(GRID_HEIGHT):
-        for column in range(GRID_WIDTH):
-            color = grid[row][column]
-            if color != 0:  # Only draw if the cell is not empty
-                pygame.draw.rect(screen, (255,255,255), pygame.Rect(column * BLOCK_WIDTH, row * BLOCK_WIDTH, BLOCK_WIDTH, BLOCK_WIDTH))
-            else:
-                pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(column * BLOCK_WIDTH, row * BLOCK_WIDTH, BLOCK_WIDTH, BLOCK_WIDTH))
-    drawGridLines()
+    #quits the program
+    pygame.quit()
+    sys.exit()
 
-    pygame.display.update()
-    
-    # Update grid only when enough time has passed
-    if update_timer >= update_interval:
-        updateGrid()
-        update_timer = 0.0
-        # Recalculate interval if speed changed
-        update_interval = 1.0 / speeds[curSpeed]
-
-
-#quits the program
-pygame.quit()
-sys.exit()
+asyncio.run(main())
